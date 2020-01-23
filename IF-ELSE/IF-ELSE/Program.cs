a56fd2db61500ba746b0161e45cd0d56ed3a7147
Mateus Silva <ryuksilva@gmail.com>
2020-01-23 17:06:45 -0300

    Condicional if-else

---
+++ IF-ELSE/IF-ELSE/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace IF_ELSE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int x = 10;
+
+
+             if (x == 10)
+             { 
+                 Console.WriteLine("Bom dia");
+             }
+             else if (x > 5) 
+             {
+                 Console.WriteLine("Boa tarde"); 
+             }
+             else {
+
+                 Console.WriteLine("Boa noite");
+
+             }*/
+
+
+            /*
+            Console.WriteLine("Entre com um numero inteiro");
+
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("Par");
+            }
+            else
+            {
+                Console.WriteLine("Impar");
+            }
+            */
+
+            Console.WriteLine("Qual a hora atual?");
+            int x = int.Parse(Console.ReadLine());
+            
+
+            if(x < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+            else if(12 <= x && x < 18)
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+            else
+            {
+                Console.WriteLine("Boa noite");
+            }
+
+
+
+
+
+
+        }
+    }
+}
---
+++ OperadoresLogicos/OperadoresLogicos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OperadoresLogicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool c1 = 4 != 5;
+            Console.WriteLine(c1); // true
+
+            bool c2 = 2 > 3 && 4 != 5;
+            Console.WriteLine(c2); // false
+
+            bool c3 = 2 > 3 || 4 != 5;
+            Console.WriteLine(c3);  // true
+
+            bool c4 = ! (2 > 3) && 4 != 5;
+            Console.WriteLine(c4); // true
+
+            bool c5 = 10 < 5;
+            bool c6 = c1 || c2 && c3;
+
+            Console.WriteLine(c6);
+
+
+
+
+        }
+    }
+}
